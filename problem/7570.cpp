85c31b35ff121de0df56a955daf8814df8b4b69e
gumgood <gumgood@naver.com>
2017-04-19 05:22:13 +0900

    Create 7570.cpp

---
+++ problem/7570.cpp
@@ -0,0 +1,17 @@
+#include<cstdio>
+#include<algorithm>
+using namespace std;
+
+int lis[1000001];
+
+int main(){
+	int n; scanf("%d", &n);
+	for(int i=0; i<n; ++i){
+		int t; scanf("%d", &t);
+		lis[t] = lis[t-1] + 1;
+	}
+	int ans = 0;
+	for(int i=1; i<=n; ++i)
+		ans = max(ans, lis[i]);
+	printf("%d", n-ans);
+}
