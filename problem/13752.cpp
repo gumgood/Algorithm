e0b0d410ef885b02ba525c2d67841b492011cdae
gumgood <gumgood@naver.com>
2018-11-07 12:52:08 +0900

    Create 13752.cpp

---
+++ problem/13752.cpp
@@ -0,0 +1,14 @@
+#include<cstdio>
+
+int main(){
+    int t;
+    scanf("%d",&t);
+    while(t--){
+        int n;
+        scanf("%d",&n);
+        while(n--)
+            printf("=");
+        puts("");
+    }
+    return 0;
+}
