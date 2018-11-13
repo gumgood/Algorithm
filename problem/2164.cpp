48e9db7e466626cb8b3fa93ddf215549940ab08d
gumgood <gumgood@naver.com>
2018-11-13 12:31:03 +0900

    Create 2164.cpp

---
+++ problem/2164.cpp
@@ -0,0 +1,19 @@
+#include<cstdio>
+#include<deque>
+using namespace std;
+
+int main(){
+    int n;
+    deque<int> dq;
+    scanf("%d",&n);
+    for(int i=1;i<=n;++i)
+        dq.push_back(i);
+    while(dq.size()>1){
+        dq.pop_front();
+        dq.push_back(dq.front());
+        dq.pop_front();
+    }
+    printf("%d",dq.front());
+    return 0;
+}
+
